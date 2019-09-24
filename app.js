30b5915b903253ffa105e9362eb68264148d3b66
ProsperInDesign <pnglazi@yahoo.com>
2019-09-24 22:24:22 +0200

    Added the data for the knowledge base

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
 let tempInputField = document.getElementById("tempInputField");
 let convertScale = document.getElementById("convertScale");
 let autoConvert = document.getElementById("auto-convert");
+const knowledgeBaseInput = document.getElementById("knowledge-base");
 
 //Buttons
 let convertButton = document.getElementById("convert");
 let resetButton = document.getElementById("reset");
 
-//Result Display
+//Displaying the results
 const celDisplay = document.getElementById("celsius");
 const fahDisplay = document.getElementById("fahrenheit");
 const kelDisplay = document.getElementById("kelvin");
 
-//Defaults the scale to the celsius on load. let because value will change
-let currentScale = convertScale.value;
+//Defaults the scale to the celsius on load. 'let' because value will change
+let currentScale;
+window.onload = () => { currentScale = convertScale.value }
 
 //Handles the calculation logic
 const handleCalculation = (value, scale) => {
@@ -26,7 +28,7 @@ const handleCalculation = (value, scale) => {
   const kelToCel = value - 273.15;
   const KelToFah = ((value - 273.15) * 9) / 5 + 32;
 
-  // Rounding down
+  // Rounding to nearest integer
   const rounded = (calculation) => Math.round(calculation * 100) / 100;
 
   switch (scale) {
@@ -46,6 +48,61 @@ const handleCalculation = (value, scale) => {
       kelDisplay.innerHTML = value;
   }
 }
+
+//Object containing knowledge base values in celsius
+const knowledgeBaseInfo = {
+  melting: {
+    silver: 961,
+    lead: 327.5,
+    tin: 231.9
+  },
+  boiling: {
+    water: 100,
+    acetone: 56,
+    ethanol: 78.37
+  },
+  freezing: {
+    water: 0,
+    acetone: -95,
+    ethanol: -114.1
+  },
+  more: {
+    sun: 5600,
+    antarctica: -100,
+    highestTemp: 56.7
+  },
+}
+
+//Validates if the field is not empty
+const convert = () => {
+  const valid = tempInputField.checkValidity();
+  if (valid) {
+    handleCalculation(+tempInputField.value, currentScale);
+  } else {
+    reset();
+    return;
+  }
+}
+
+const reset = () => {
+  tempInputField.value = null;
+  celDisplay.innerHTML = '--';
+  fahDisplay.innerHTML = '--';
+  kelDisplay.innerHTML = '--';
+}
+
+const showKnowledgeBaseInfo = (e) => {
+  const categories = e.target.children;
+  const catArr = [];
+  for (category of categories) {
+    catArr.push(category.id)
+  };
+  for (prop in knowledgeBaseInfo) {
+    const match = catArr.find(e => prop == e);
+    console.log(knowledgeBaseInfo[prop]);
+  }
+
+};
 
 //EVENT LISTENERS
 //a. Sets the scale to the select one
@@ -70,24 +127,6 @@ autoConvert.addEventListener('change', () => {
   }
 });
 
-//Validates if the field is not empty
-const convert = () => {
-  const valid = tempInputField.checkValidity();
-  if (valid) {
-    handleCalculation(+tempInputField.value, currentScale);
-  } else {
-    reset();
-    return;
-  }
-}
-
-const reset = () => {
-  tempInputField.value = null;
-  celDisplay.innerHTML = '--';
-  fahDisplay.innerHTML = '--';
-  kelDisplay.innerHTML = '--';
-}
-
 // Runs the convert function when button clicked 
 convertButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -96,3 +135,4 @@ convertButton.addEventListener("click", (e) => {
 
 //Resets the app values to their defaults
 resetButton.addEventListener("click", reset);
+knowledgeBaseInput.addEventListener('change', showKnowledgeBaseInfo);
