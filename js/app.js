1a0a8980b6ced63f7ebb2c5dc6717aef490e2a22
Prosper <prosperinweb@gmail.com>
2020-04-17 22:28:03 +0200

    Update styles and separate css and js names

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Input Elements
-let tempInputField = document.getElementById("tempInputField");
-let convertScale = document.getElementById("convertScale");
+let tempInputField = document.getElementById("temp-input-field");
+let convertScale = document.getElementById("convert-scale");
 let autoConvert = document.getElementById("auto-convert");
 const knowledgeBaseInput = document.getElementById("knowledge-base");
 
@@ -15,12 +15,12 @@ const kelDisplay = document.getElementById("kelvin");
 
 //Defaults the scale to the celsius on load. 'let' because value will change
 let currentScale;
-window.onload = () => { currentScale = convertScale.value }
-
+window.onload = () => {
+  currentScale = convertScale.value;
+};
 
 //Handles the calculation logic
 const handleCalculation = (value, scale) => {
-
   //a. The calculations
   const celToFah = value * (9 / 5) + 32;
   const celToKel = value + 273.15;
@@ -33,69 +33,48 @@ const handleCalculation = (value, scale) => {
   const rounded = (calculation) => Math.round(calculation * 100) / 100;
 
   switch (scale) {
-    case 'celsius':
+    case "celsius":
       celDisplay.innerHTML = value;
       fahDisplay.innerHTML = rounded(celToFah);
       kelDisplay.innerHTML = rounded(celToKel);
       break;
-    case 'fahrenheit':
+    case "fahrenheit":
       celDisplay.innerHTML = rounded(fahToCel);
       fahDisplay.innerHTML = value;
       kelDisplay.innerHTML = rounded(fahToKel);
       break;
-    case 'kelvin':
+    case "kelvin":
       celDisplay.innerHTML = rounded(kelToCel);
       fahDisplay.innerHTML = rounded(KelToFah);
       kelDisplay.innerHTML = value;
   }
-}
+};
 
 const isValid = (element) => {
-  element.checkValidity() ? element.classList.remove('invalid') : element.classList.add('invalid');
+  element.checkValidity()
+    ? element.classList.remove("invalid")
+    : element.classList.add("invalid");
   return element.checkValidity();
-
-}
+};
 
 //Validates if the field is not empty then runs calculations
 const convert = () => {
-  isValid(tempInputField) ? handleCalculation(+tempInputField.value, currentScale) : '';
-}
+  isValid(tempInputField)
+    ? handleCalculation(+tempInputField.value, currentScale)
+    : "";
+};
 
 const reset = () => {
   tempInputField.value = null;
-  celDisplay.innerHTML = '--';
-  fahDisplay.innerHTML = '--';
-  kelDisplay.innerHTML = '--';
-  tempInputField.classList.remove('invalid');
-}
-
-const knowledgeBaseInfo = {
-  melting: {
-    "silver": 961,
-    "lead": 327.5,
-    "tin": 231.9
-  },
-  boiling: {
-    "water": 100,
-    "acetone": 56,
-    "ethanol": 78.37
-  },
-  freezing: {
-    "water": 0,
-    "acetone": -95,
-    "ethanol": -114.1
-  },
-  more: {
-    "sun": 5600,
-    "antarctica": -100,
-    "highestTemp": 56.7
-  }
-}
+  celDisplay.innerHTML = "--";
+  fahDisplay.innerHTML = "--";
+  kelDisplay.innerHTML = "--";
+  tempInputField.classList.remove("invalid");
+};
 
 const showKnowledgeBaseInfo = (e) => {
   const categories = e.target.children;
   console.log(categories);
-
 };
 
 //EVENT LISTENERS
@@ -105,12 +84,12 @@ convertScale.addEventListener("change", () => {
 });
 
 /*
-1. When checked, it adds an event listener to the input field, running convert() for every change or keyup
+1. When checked, it adds an event listener to the input field, running convert() for:
   a. The 'change' event for when the numeric sliders are used
   b. keyup for when the keyboard is used
 2. When unchecked, it removes the input ELs or else the automatic conversion will keep happening
 */
-autoConvert.addEventListener('change', () => {
+autoConvert.addEventListener("change", () => {
   if (autoConvert.checked) {
     tempInputField.addEventListener("keyup", convert);
     tempInputField.addEventListener("change", convert);
@@ -120,7 +99,7 @@ autoConvert.addEventListener('change', () => {
   }
 });
 
-// Runs the convert function when button clicked 
+// Runs the convert function when button clicked
 convertButton.addEventListener("click", (e) => {
   e.preventDefault();
   convert();
@@ -128,5 +107,3 @@ convertButton.addEventListener("click", (e) => {
 
 //Resets the app values to their defaults
 resetButton.addEventListener("click", reset);
-
-knowledgeBaseInput.addEventListener('change', showKnowledgeBaseInfo);
